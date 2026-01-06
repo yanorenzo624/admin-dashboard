7e12a924c5cacbcf426139e5dd50e390c2ee6c7c
yanorenzo624 <yanorenzo624@gmail.com>
2026-01-06 09:33:07 -0800

    created mock data.

---
+++ src/data/mockData.js
@@ -0,0 +1,27 @@
+export const stats = [
+  {
+    title: "Users",
+    value: "1,245",
+  },
+  {
+    title: "Orders",
+    value: "856",
+  },
+  {
+    title: "Revenue",
+    value: "$12,430",
+  },
+  {
+    title: "Growth",
+    value: "+12%",
+  },
+];
+
+export const salesData = [
+  { name: "Jan", sales: 400 },
+  { name: "Feb", sales: 300 },
+  { name: "Mar", sales: 500 },
+  { name: "Apr", sales: 450 },
+  { name: "May", sales: 600 },
+  { name: "Jun", sales: 700 },
+];

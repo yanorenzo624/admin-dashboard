993986e67231aa9a4d05b4b5cf5b3065d60a6acd
yanorenzo624 <yanorenzo624@gmail.com>
2026-01-06 09:17:44 -0800

    setup app routing.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-function App() {
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar";
 
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <BrowserRouter>
+      <div className="flex min-h-screen bg-gray-100">
+        <Sidebar />
 
-export default App
+        <div className="flex-1 flex flex-col">
+          <Topbar />
+
+          <main className="p-6">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
+          </main>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default App;
